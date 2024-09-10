256afa66f14ea8b55b1403a3519c26e4403d8844
sariya <dayzwaitik123@yandex.ru>
2024-09-10 21:56:02 +0300

    negative test cases

--- internal/quadratic/quadratic_test.go
+++ internal/quadratic/quadratic_test.go
@@ -1,6 +1,7 @@
 package quadratic_test
 
 import (
+	"fmt"
 	"gofgen/internal/quadratic"
 	"testing"
 
@@ -47,40 +48,63 @@ func TestCreateNewEquationSuccess(t *testing.T) {
 			},
 			expectedErr: nil,
 		},
-		// {
-		// 	caseName:    "coef by x^2 is zero. Error",
-		// 	coefs:       map[string]float64{"a": 0, "b": -3.1, "c": 4.9},
-		// 	undefVar:    "x",
-		// 	expectedEq:  nil,
-		// 	expectedErr: fmt.Errorf("coefficient at the leading term is zero"),
-		// },
-		// {
-		// 	caseName:    "To much coefs",
-		// 	coefs:       map[string]float64{"a": 0, "b": -3.1, "c": 4.9, "d": 3.3},
-		// 	undefVar:    "x",
-		// 	expectedEq:  nil,
-		// 	expectedErr: fmt.Errorf("need 3 coefficients, got 4"),
-		// },
-		// {
-		// 	caseName:    "No enough coefs",
-		// 	coefs:       map[string]float64{"a": 0},
-		// 	undefVar:    "x",
-		// 	expectedEq:  nil,
-		// 	expectedErr: fmt.Errorf("need 3 coefficients, got 1"),
-		// },
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.caseName, func(t *testing.T) {
 			res, err := quadratic.New(tc.coefs, tc.undefVar)
 			if err != nil {
-				t.Errorf("got unexpected error: %v", err)
+				t.Errorf("got unexpected error: (%v)", err)
 			}
 			if diff := cmp.Diff(*tc.expectedEq, *res); diff != "" {
 				t.Fatal(diff)
 			}
 			if res.UndefinedVariable != tc.undefVar {
-				t.Errorf("Expected %s, got %s", res.UndefinedVariable, tc.undefVar)
+				t.Errorf("Expected: (%s), got: (%s)", res.UndefinedVariable, tc.undefVar)
+			}
+		})
+	}
+}
+
+func TestCreateNewQuadraticNegative(t *testing.T) {
+	testCases := []struct {
+		caseName    string
+		coefs       map[string]float64
+		undefVar    string
+		expectedEq  *quadratic.QuadraticEquation
+		expectedErr error
+	}{
+		{
+			caseName:    "coef by x^2 is zero. Error",
+			coefs:       map[string]float64{"a": 0, "b": -3.1, "c": 4.9},
+			undefVar:    "x",
+			expectedEq:  nil,
+			expectedErr: fmt.Errorf("coefficient at the leading term is zero"),
+		},
+		{
+			caseName:    "To much coefs",
+			coefs:       map[string]float64{"a": 0, "b": -3.1, "c": 4.9, "d": 3.3},
+			undefVar:    "x",
+			expectedEq:  nil,
+			expectedErr: fmt.Errorf("need 3 coefficients, got %d", 4),
+		},
+		{
+			caseName:    "No enough coefs",
+			coefs:       map[string]float64{"a": 0},
+			undefVar:    "x",
+			expectedEq:  nil,
+			expectedErr: fmt.Errorf("need 3 coefficients, got %d", 1),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.caseName, func(t *testing.T) {
+			res, err := quadratic.New(tc.coefs, tc.undefVar)
+			if res != nil {
+				t.Fatalf("unexpected creating equation")
+			}
+			if err.Error() != tc.expectedErr.Error() {
+				t.Errorf("got unexpected error. Expect: (%v), got: (%v)", tc.expectedErr, err)
 			}
 		})
 	}
