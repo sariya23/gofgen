236c5b002f254186eb7136bd31171e9223fa9922
sariya <dayzwaitik123@yandex.ru>
2024-09-11 14:57:05 +0300

    add doc string

--- pkg/quadratic/quadratic.go
+++ pkg/quadratic/quadratic.go
@@ -7,6 +7,8 @@ import (
 	"github.com/shopspring/decimal"
 )
 
+// QuadraticEquation хранит основную информацию о квадратном уравнении.
+// Такую как коэффициенты, ответ, название неизвестной переменной.
 type QuadraticEquation struct {
 	A, B, C, Answer   decimal.Decimal
 	UndefinedVariable string
@@ -30,6 +32,8 @@ func isLeadTermIsZero(coefficient [3]decimal.Decimal) bool {
 	return coefficient[0].Equal(decimal.NewFromFloat(0))
 }
 
+// String возвращает сторокове представление квадратного уравнения.
+// Например, 3.4x² + 5x = 4.
 func (qe QuadraticEquation) String() string {
 	stringEquation := fmt.Sprintf("%v%v²", qe.A.String(), qe.UndefinedVariable)
 	if qe.B.Compare(decimal.NewFromFloat(0.0)) != 0 {
